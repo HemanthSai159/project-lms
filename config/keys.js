699e084ac6486f2646ddfac110f0a186af2408bf
Koppolu Venkata Hemanth Sai <99526602+HemanthSai159@users.noreply.github.com>
2022-05-15 15:46:34 +0530

    Delete config directory

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-
-dbPassword ='mongodb+srv://userlog:' + encodeURIComponent('12345') + '@userlogins.gkbt5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-module.exports = {
-    mongoURI: dbPassword
-};
