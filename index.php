c6c591ab14473bd21991a1234f29a28d8c10e339
Koppolu Venkata Hemanth Sai <99526602+HemanthSai159@users.noreply.github.com>
2022-05-15 15:54:42 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eLearn</title>
+    <link rel="stylesheet" href="css/stylms.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <a href="index.php" style="text-decoration: none;">
+            <h1 class='logo'>eLearn</h1>
+        </a>
+    </header>
+
+    <main>
+        <div class="hero-content">
+            <p class="p1">Education is your right</p>
+            <p class="p1" style="margin-top: -16px">Take it now!</p>
+            <p class="p2">Join Us To Learn New Things On The Go</p>
+            <a href="pages/signup.php">
+                <input class='signup-butt' type="button" value="Sign Up"></a>
+            <a href="pages/login.php">
+                <input class='login-butt' type="button" value="Login"></a>
+        </div>
+    </main>
+
+    <footer>
+        <p><small>Copyright &copy; 2022 eLearn</small></p>
+    </footer>
+</body>
+
+</html>
